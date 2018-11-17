84404385981ba5f039f9eab55ac8c3c994e32658
Sonam Ghale <sonamghale2@gmail.com>
2018-11-17 20:16:30 +0000

    update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,11 +4,18 @@ class TodoController<ApplicationController
     end
     def show
         @todo_description = "whatever"
-        @todo_pomodoro_estimate =6
-        todo_id ='1'
+        @todo_pomodoro_estimate = 6
+        todo_id = params[:id]
         if todo_id =='1'
-            @todo_description ="stop"
-        elseif todo_id =='2'
+            @todo_description = "Get To School On Time"
+            @todo_pomodoro_estimate = 1
+            
+        elsif todo_id =='2'
+            @todo_description = "Passed in Homeworks"
+              @todo_pomodoro_estimate = 30
+        elseif todo_id =='3'
+            @todo_description = "Get Home Before 5"
+            @todo_pomodoro_estimate = 60
         end    
     end
 end
