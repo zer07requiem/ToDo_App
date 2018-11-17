517b793353216ee7dc2a6725ed9741b423c00be6
Sonam Ghale <sonamghale2@gmail.com>
2018-11-17 19:13:45 +0000

    update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,6 +5,10 @@ class TodoController<ApplicationController
     def show
         @todo_description = "whatever"
         @todo_pomodoro_estimate =6
+        todo_id ='1'
+        if todo_id =='1'
+            @todo_description ="stop"
+        elseif todo_id =='2'
+        end    
     end
-
 end
