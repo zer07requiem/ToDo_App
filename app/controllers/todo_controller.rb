64c268c6bc0392b681b32cbf2411d850771c475b
Sonam Ghale <sonamghale2@gmail.com>
2018-11-18 19:14:00 +0000

    update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,17 +4,15 @@ class TodoController<ApplicationController
     end
     def show
         @todo=Todo.find_by_id(params[:id])
-        @todo_description = "whatever"
-        @todo_pomodoro_estimate = 6
-        todo_id = params[:id]
-        if todo_id =='1'
+        @todo_id = params[:id]
+        if @todo_id =='1'
             @todo_description = "Get To School On Time School On Time"
             @todo_pomodoro_estimate = 1
             
-        elsif todo_id =='2'
+        elsif @todo_id =='2'
             @todo_description = "Passed in Homeworks"
             @todo_pomodoro_estimate = 1
-        elsif todo_id =='3'
+        elsif @todo_id =='3'
             @todo_description = "Daily Grind"
             @todo_pomodoro_estimate = 5
         end
@@ -27,5 +25,20 @@ class TodoController<ApplicationController
         t.pomodoro_estimate = params['pomodoro_estimate']
         t.save
         redirect_to "/todo/show/#{t.id}"
+    end
+    def edit
+          @todo = Todo.find_by_id(params[:id])
+    end
+    def update
+        t = Todo.find_by_id(params['id'])
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
+    def destroy
+        t = Todo.find_by_id(params[:id])
+        t.destroy
+        redirect_to "/todo/index"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ get 'todo/index', to:'todo#index'
 get 'todo/show/:id', to:'todo#show'
 get 'todo/new', to:'todo#new'
 get 'todo/create', to:'todo#create'
+get 'todo/edit/:id', to: 'todo#edit'
+get 'todo/update/:id', to: 'todo#update'
+get 'todo/destroy/:id', to: 'todo#destroy'
 end
