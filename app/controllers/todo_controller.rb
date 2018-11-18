d7c1dd585e9857445528e7594bd55722c7f8c355
Sonam Ghale <sonamghale2@gmail.com>
2018-11-18 18:28:50 +0000

    update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,19 +3,29 @@ class TodoController<ApplicationController
     def index
     end
     def show
+        @todo=Todo.find_by_id(params[:id])
         @todo_description = "whatever"
         @todo_pomodoro_estimate = 6
         todo_id = params[:id]
         if todo_id =='1'
-            @todo_description = "Get To School On Time"
+            @todo_description = "Get To School On Time School On Time"
             @todo_pomodoro_estimate = 1
             
         elsif todo_id =='2'
             @todo_description = "Passed in Homeworks"
-              @todo_pomodoro_estimate = 30
-        elseif todo_id =='3'
-            @todo_description = "Get Home Before 5"
-            @todo_pomodoro_estimate = 60
-        end    
+            @todo_pomodoro_estimate = 1
+        elsif todo_id =='3'
+            @todo_description = "Daily Grind"
+            @todo_pomodoro_estimate = 5
+        end
+    end
+    def new
+    end
+    def create
+        t=Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
     
 get 'todo/index', to:'todo#index'
 get 'todo/show/:id', to:'todo#show'
-
+get 'todo/new', to:'todo#new'
+get 'todo/create', to:'todo#create'
 end
